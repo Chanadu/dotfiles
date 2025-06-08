d33aad674b78a2320d8fdb7ab2806f18bf88005b
Chanadu <chandu.peddada@gmail.com>
2025-06-08 00:59:35 -0500

    initial commit 2.0

---
+++ .config/fish/config.fish
@@ -0,0 +1,62 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+# alias p 'paru && sudo fixBootLoader'
+# alias pis 'paru -Ss --searchby name'
+# alias ps 'paruz'
+# alias pi 'paru -S'
+# alias pr 'paru -R'
+# alias prs 'paru -Rs'
+
+#alias air '~/go/bin/air'
+#alias fyne '~/go/bin/fyne'
+#alias wails '~/go/bin/wails'
+
+fish_add_path ~/go/bin
+fish_add_path ~/Folders/CustomPrograms
+fish_add_path ~/Folders/CustomPrograms/pros-cli-linux
+fish_add_path ~/Folders/CustomPrograms/CEDev/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/update.sh
+
+set -x BROWSER '/usr/bin/firefox-nightly'
+set -x EDITOR '/usr/bin/nvim'
+# set -x TERMINAL '/usr/bin/kitty'
+set -x TERMINAL '/usr/bin/alacritty'
+set -x CAPACITOR_ANDROID_STUDIO_PATH '/opt/android-studio/bin/studio.sh'
+set -x XDG_CURRENT_DESKTOP sway
+set -x XDG_SESSION_DESKTOP sway
+set -x XDG_SESSION_TYPE wayland
+# set -x XDG_RUNTIME_DIR '/run/user/$UID'
+# set -x DBUS_SESSION_BUS_ADDRESS 'unix:path=${XDG_RUNTIME_DIR}/bus'
+
+
+set -xU __done_min_cmd_duration 5000
+
+zoxide init --cmd cd fish | source
+#silver init | source
+#starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/chanadu/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# while nvim .code === 0
+#   echo "restarting" 
+# end
+set -u pure_show_job true
+set -u pure_enable_k8s true
+set -u pure_enable_nixdevshell true
+set -u pure_separate_prompt_on_error true
+set -u pure_symbol_ssh_prefix "ssh:"
+set -u pure_show_system_time true
+set -u pure_show_subsecond_command_duration true
+set -u pure_threshold_command_duration .25
+set -u pure_show_prefix_root_prompt true
+set -u pure_color_info blue
+set -u pure_color_primary cyan
+
---
+++ .config/nixos/conf.nix
@@ -0,0 +1,225 @@
+
+
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+	imports = [];
+
+	# nix.settings.auto-optimize-store = true;
+	nix.gc = {
+		automatic = true;
+		dates = "daily";
+	};
+
+	# Bootloader.
+	boot.loader.systemd-boot = {
+		enable = true;
+		configurationLimit = 3;
+	};
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	networking.hostName = "chanadu-desktop"; # Define your hostname.
+	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+	# Configure network proxy if necessary
+	# networking.proxy.default = "http://user:password@proxy:port/";
+	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+	# Enable networking
+	networking.networkmanager.enable = true;
+
+	# Set your time zone.
+	time.timeZone = "America/Chicago";
+
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
+
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_US.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE = "en_US.UTF-8";
+		LC_TIME = "en_US.UTF-8";
+	};
+
+	hardware.graphics = {
+		enable = true;
+	};
+
+	# Configure keymap in X11
+	services.xserver.xkb = {
+		layout = "us";
+		variant = "";
+	};
+
+	services.xserver.enable = true;
+	# services.displayManager.defaultSession = "swayfx";
+
+	# Define a user account. Don't forget to set a password with ‘passwd’.
+	users = {
+		defaultUserShell = pkgs.fish;
+		users.chanadu = {
+			isNormalUser = true;
+			description = "Chanadu";
+			extraGroups = [ "networkmanager" "wheel" ];
+			packages = with pkgs; [];
+			shell = pkgs.fish;
+		};
+	};
+
+	# Allow unfree packages
+	nixpkgs.config.allowUnfree = true;
+
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		# bspwm
+		# sxhkd
+		# hyprland
+		swayfx-unwrapped
+		vim
+		wget
+		neovim
+		# refind
+		# efibootmgr
+		# sbsigntool
+		alacritty
+		fish
+		waybar
+		# greetd.greetd
+		ly
+		firefox
+		rofi
+		pipewire
+		wireplumber
+		mako
+		git
+		xfce.thunar
+		p7zip
+		zoxide
+		kubectl
+		gcc
+		rocmPackages.llvm.clang-unwrapped
+		zig
+		go
+		zulu
+		fzf
+		gnumake
+		obsidian
+		nodejs_24
+		cmake
+		# kitty
+		playerctl
+		xdg-desktop-portal-wlr
+		polkit
+		# gtk4
+		# gtk3
+		# gtk2
+		grim
+		wl-clipboard
+		neofetch
+		autotiling
+		spotify
+		discord
+		ripgrep
+		stow
+		lazygit
+		vscode
+		fd
+	];
+
+	fonts.packages = with pkgs; [
+		nerd-fonts.monaspace
+	];
+
+	# gtk = {
+	# 	enable = true;
+	# 	font.name = "MonaspiceNe Nerd Font Mono";
+	#
+	# 	iconTheme = {
+	# 		name = "Papirus-Dark";
+	# 		package = pkgs.papirus-icon-theme;
+	# 	};
+	#
+	# 	gtk3.extraConfig = {
+	# 		Settings = ''
+	# 			gtk-application-prefer-dark-theme=1
+	# 		'';
+	# 	};
+	#
+	# 	gtk4.extraConfig = {
+	# 		Settings = ''
+	# 			gtk-application-prefer-dark-theme=1
+	# 		'';
+	# 	};
+	# };
+
+	# services.xserver.windowManager.bspwm.configFile = "/home/chanadu/.config/bspwm/bspwmrc.sh";
+	# services.xserver.windowManager.bspwm.sxhkd.configFile = "/home/chanadu/.config/sxhkd/sxkhdrc.sh";
+
+
+	security.rtkit.enable = true;
+
+	security.polkit.enable = true;
+
+	services.gnome.gnome-keyring.enable = true;
+	  
+
+	# Some programs need SUID wrappers, can be configured further or are
+	# started in user sessions.
+	# programs.mtr.enable = true;
+	# programs.gnupg.agent = {
+	#   enable = true;
+	#   enableSSHSupport = true;
+	# };
+
+	# programs.hyprland.enable = true;
+
+	programs.sway = {
+		enable = true;
+		package = pkgs.swayfx;
+		wrapperFeatures.gtk = true;
+	};
+
+	programs.fish.enable = true;
+
+	# List services that you want to enable:
+
+	# Enable the OpenSSH daemon.
+	services.openssh.enable = true;
+
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		jack.enable = true;
+	};
+
+
+
+	environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+	# Open ports in the firewall.
+	# networking.firewall.allowedTCPPorts = [ ... ];
+	# networking.firewall.allowedUDPPorts = [ ... ];
+	# Or disable the firewall altogether.
+	# networking.firewall.enable = false;
+
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "25.05"; # Did you read the comment?
+
+}
