73530ea90bc6952e49c122852d7d3e36495e8f5b
Chanadu <chandu.peddada@gmail.com>
2025-06-10 21:04:24 -0500

    more changes

--- .config/nixos/conf.nix
+++ .config/nixos/conf.nix
@@ -28,8 +28,8 @@ in
 	boot.loader = {
 		timeout = 120;
 		systemd-boot = {
-			# enable = lib.mkForce false;
-			enable = true;
+			enable = lib.mkForce false;
+			# enable = true;
 			configurationLimit = 1;
 		};
 	};
@@ -72,12 +72,13 @@ in
 
 	# boot.loader.efi.efiSysMountPoint = "/efi";
 	# boot.loader.systemd-boot.xbootldrMountPoint = "/boot";
-		# boot.lanzaboote = {
-	# 	enable = true;
-	# 	pkiBundle = "/var/lib/sbctl";
-	# };
+	boot.lanzaboote = {
+		enable = true;
+		# pkiBundle = "/var/lib/sbctl";
+		pkiBundle = "/var/lib/sbctl";
+	};
 	# boot.loader.systemd-boot.enable = false;
-	#
+
 	# boot.loader.grub = {
 	# 	enable = true;
 	# 	device = "nodev";
@@ -268,6 +269,7 @@ in
 		luajitPackages.luarocks
 		ruff
 		pyright
+		libpam-wrapper
 	];
 
 	fonts.packages = with pkgs; [
@@ -322,25 +324,25 @@ in
 	powerManagement.enable = true;
 	services.thermald.enable = true;
 	
-	services.tlp = {
-		enable = true;
-		settings = {
-			CPU_SCALING_GOVERNOR_ON_AC = "performance";
-			CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-			CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-			CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-			CPU_MIN_PERF_ON_AC = 0;
-			CPU_MAX_PERF_ON_AC = 100;
-			CPU_MIN_PERF_ON_BAT = 0;
-			CPU_MAX_PERF_ON_BAT = 100;
-
-			# Optional helps save long term battery health
-			START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-			STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
-		};
-	};
+	# services.tlp = {
+	# 	enable = true;
+	# 	settings = {
+	# 		CPU_SCALING_GOVERNOR_ON_AC = "performance";
+	# 		CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+	#
+	# 		CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+	# 		CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+	#
+	# 		CPU_MIN_PERF_ON_AC = 0;
+	# 		CPU_MAX_PERF_ON_AC = 100;
+	# 		CPU_MIN_PERF_ON_BAT = 0;
+	# 		CPU_MAX_PERF_ON_BAT = 100;
+	#
+	# 		# Optional helps save long term battery health
+	# 		START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+	# 		STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+	# 	};
+	# };
 
 	# services.greetd = {
 	# 	enable = true;
